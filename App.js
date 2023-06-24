bca0bc7e823c19537329bede528920e6c62bb56e
Ally Juma Said <mahadsaid95@icloud.com>
2023-06-24 05:34:46 +0100

    studied about View,Text and Styles component

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Header</Text>
+      </View>
       <StatusBar style="auto" />
+      <View style={styles.body}>
+        <Text style={styles.boldText}>MuhibulHaq Said Ally  </Text>
+        <Text style={styles.boldText}>lorem ipsum emit aves cartex is </Text>
+        <Text style={styles.boldText}>lorem ipsum emit aves cartex is </Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff0",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  header :{
+    backgroundColor: "#f00",
+    padding: 20,
+    borderRadius: 9,
+  },
+  boldText: {
+    fontWeight: "bold"
+  },
+  body: {
+    backgroundColor: "#0f0",
+    padding: 20,
+    borderRadius:15
+  }
 });
