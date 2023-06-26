44c2d7c43fdab4d9b285c1174f20e8995e107aae
Ally Juma Said <mahadsaid95@icloud.com>
2023-06-26 02:30:21 +0100

    learning the way of writing props

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View,Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 import React, { useState } from "react";
 
-
 export default function App() {
+  const [name, setName] = useState("MuhibulHaq Said Ally");
+  const [age, setAge] = useState("30");
 
-  const [name, setName] = useState("MuhibulHaq Said Ally"); 
-  const [person, setPerson] = useState({name: "MuhibulHaq", age: 40});
-
-
-   const clickHandler = () => {
-      setName("MuhibulHaq Said");
-      setPerson({name: "Muhibul", age: 46});
-    }
-   
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Header</Text>
-        {/* <Text style={styles.boldText}>My name is {name}</Text> */}
-      </View>
-      <StatusBar style="auto" />
-      <View style={styles.body}>
-        <Text style={styles.boldText}>his name is{person.name} and his age is {person.age}</Text> 
-        <Text style={styles.boldText}>My name is {name}</Text>
-        <Text style={styles.boldText}>lorem ipsum emit aves cartex is </Text>
-        <Text style={styles.boldText}>lorem ipsum emit aves cartex is </Text>
-      </View>
-      <View style={styles.buttonContainer}>
-      <Button title="update state" onPress= {clickHandler} />
-      </View>
+      <Text>Enter Name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder="Enter Name e.g Don Doe"
+        onChangeText={(name) => {
+          setName(name);
+        }}
+      />
+      {/* age */}
+      <Text>Enter Age:</Text>
+      <TextInput
+      keyboardType="numeric"
+        style={styles.input}
+        placeholder="Enter age e.g 12"
+        onChangeText={(age) => {
+          setAge(age);
+        }}
+      />
+      <Text>
+        name:{name} , age:{age}
+      </Text>
     </View>
   );
 }
@@ -41,20 +41,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  header :{
+  header: {
     backgroundColor: "#f00",
     padding: 20,
     borderRadius: 9,
   },
   boldText: {
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   body: {
     backgroundColor: "#0f0",
     padding: 20,
-    borderRadius:15
+    borderRadius: 15,
   },
-  buttonContainer:{
-    marginTop:20,
-  }
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 15,
+    width: 200,
+  },
 });
