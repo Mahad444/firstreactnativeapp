1f326f8cd7d7c74f32a82c7e178194f2fe242b0f
Ally Juma Said <mahadsaid95@icloud.com>
2024-11-16 21:57:24 -0700

    func

--- functions.js
+++ functions.js
@@ -9,3 +9,12 @@ function getCurrentDate() {
     var year = date.getFullYear();
     return year + "-" + month + "-" + day;
 }
+
+// Function to get the current time
+function getCurrentTime() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    return hours + ":" + minutes + ":" + seconds;
+}
