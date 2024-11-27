5dbe9ed1d162127a2ba7b7fdb8055a0f4665918e
Ally Juma Said <mahadsaid95@icloud.com>
2024-11-26 22:25:09 -0900

    geocoding

--- functions.js
+++ functions.js
@@ -21,5 +21,9 @@ function getCurrentTime() {
 
 //function to check location
 function getLocation(){
-    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
 }
