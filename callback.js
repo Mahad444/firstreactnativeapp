8f4e154957f509a85c5b6088a73689b0112ace76
Ally Juma Said <mahadsaid95@icloud.com>
2024-12-10 20:37:01 -0700

    callback

---
+++ callback.js
@@ -0,0 +1,8 @@
+// callback 
+// callback is a function that is passed as an argument to another function
+
+// function that takes a callback function as an argument
+function doSomething(callback) {
+  // call the callback function
+  callback();
+}
